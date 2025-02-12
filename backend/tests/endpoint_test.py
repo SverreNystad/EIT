176d81c2f5f64a033242667e19161038d201d5ce
SverreNystad <sverrenystad@gmail.com>
2025-02-12 13:54:49 +0100

    test: add test for GET message endpoint

---
+++ backend/tests/endpoint_test.py
@@ -0,0 +1,14 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_get_message():
+    """
+    Test file upload with different authentication tokens
+    """
+    response = client.get("/message")
+    assert response.status_code == 200
