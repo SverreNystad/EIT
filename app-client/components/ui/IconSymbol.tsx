fc7901dcec79408484f68a3dc7bc1805ef0e6d74
serinaerzengin <s364561@oslomet.no>
2025-03-19 19:55:16 +0100

    fix: new icon in oppskrifter tab

--- app-client/components/ui/IconSymbol.tsx
+++ app-client/components/ui/IconSymbol.tsx
@@ -10,11 +10,12 @@ const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
   'house.fill': 'home',
-  'paperplane.fill': 'send',
+  'fork.knife': 'restaurant',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
   'list.bullet': 'list',
   'person.fill': 'person',
+  
 
 } as Partial<
   Record<
